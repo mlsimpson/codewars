d61c86d6dd012d22b6ee6d4b65c3fb5c5a7ce77f
mlsimpson <maui@threv.net>
2022-01-25 15:24:06 -0500

    sort_the_odd

---
+++ sort_the_odd.py
@@ -0,0 +1,21 @@
+def sort_array(source_array):
+
+    out = []
+    evens = [x for x in source_array if x % 2 == 0]
+    odds = sorted([x for x in source_array if x % 2 != 0])
+
+    # print(odds)
+
+    for x in source_array:
+        if(x % 2 == 0):
+            out.append(evens.pop(0))
+        else:
+            out.append(odds.pop(0))
+
+    return out
+
+
+print(sort_array([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))
+
+
+
