d583a81fe74bb395b080c51d7ff2e294e4892c4f
mlsimpson <maui@threv.net>
2022-02-03 03:52:40 -0500

    rps

---
+++ rock_paper_scissors.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/5672a98bdbdd995fad00000f/solutions/python
+
+def rps(p1, p2):
+    if p1 == "rock" and p2 == "scissors":
+        return "Player 1 won!"
+    elif p1 == "scissors" and p2 == "paper":
+        return "Player 1 won!"
+    elif p1 == "paper" and p2 == "rock":
+        return "Player 1 won!"
+    elif p1 == p2:
+        return "Draw!"
+    else:
+        return "Player 2 won!"
+
