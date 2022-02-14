e4973348e28bb74f7cbc6c8370dd918cd678f5e1
mlsimpson <maui@threv.net>
2022-02-13 22:54:49 -0500

    reversing a process, wow tho

--- 6kyu/reversing_a_process.py
+++ 6kyu/reversing_a_process.py
@@ -26,4 +26,23 @@ def decode(r):
         return "Impossible to decode"
     return res
 
-
+# best practices solutions from codewars:
+#
+# def decode(r):
+#     i = next(i for i, c in enumerate(r) if c.isalpha())
+#     n = int(r[:i]) % 26
+#     if n == 13 or n % 2 == 0: return "Impossible to decode"
+#     d = {ascii_lowercase[n * i % 26]: c for i, c in enumerate(ascii_lowercase)}
+#     return ''.join(d[c] for c in r[i:])
+#
+# import re
+# def decode(s) :
+#     nb, s = re.findall('[0-9]+|[a-z]+', s)
+#     inv = [x for x in range(26) if int(nb) * x % 26 == 1]
+#     if len(inv) != 1 : return 'Impossible to decode'
+#     res = ""
+#     iv = inv[0]
+#     alpha = "abcdefghijklmnopqrstuvwxyz"
+#     for c in s:
+#         res += alpha[alpha.index(c) * iv % 26]
+#     return res
